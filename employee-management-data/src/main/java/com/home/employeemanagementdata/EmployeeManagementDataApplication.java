7a38d7e26e637acd9628abcef846f94046d3942e
Rahul <rsingh33@buffalo.edu>
2020-05-09 00:45:55 -0400

    Initial Multi Module Maven project set up

---
+++ employee-management-data/src/main/java/com/home/employeemanagementdata/EmployeeManagementDataApplication.java
@@ -0,0 +1,13 @@
+package com.home.employeemanagementdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagementDataApplication.class, args);
+    }
+
+}
---
+++ employee-management-web/src/test/java/com/hom/employeemanagementweb/EmployeeManagementWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hom.employeemanagementweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
