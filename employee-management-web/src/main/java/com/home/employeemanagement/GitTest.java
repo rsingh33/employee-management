edc0211eef32bb1e789f1b1974ccca4c618f9119
Rahul <rsingh33@buffalo.edu>
2020-05-30 18:30:24 -0400

    Removed random git values

--- employee-management-web/src/main/java/com/home/employeemanagement/GitTest.java
+++ employee-management-web/src/main/java/com/home/employeemanagement/GitTest.java
@@ -4,11 +4,9 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git status");
         System.out.println("git status 2222");
-<<<<<<< HEAD
         System.out.println("git status 333");
         System.out.println("git checkout master");
-=======
         System.out.println("git branch develop");
->>>>>>> develop
+
     }
 }
