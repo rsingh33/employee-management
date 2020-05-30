429ede217b2c36cc92794abe7e263f94978ef2ba
Rahul <rsingh33@buffalo.edu>
2020-05-30 17:54:56 -0400

    updated git ignore and Git test

--- employee-management-web/src/main/java/com/home/employeemanagement/GitTest.java
+++ employee-management-web/src/main/java/com/home/employeemanagement/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git status");
         System.out.println("git status 2222");
+        System.out.println("git status 333");
     }
 }
